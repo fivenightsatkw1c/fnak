f75b2d49a75b2a167dfa06386928526b9fae0872
Troy Sleeuwen <troy.vansleeuwen@edu-kw1c.nl>
2024-01-12 15:16:24 +0100

    fixed path hope

--- Database/StudentController.php
+++ Database/StudentController.php
@@ -9,7 +9,7 @@ Subject:    General functies voor de database
 // Om deze code te gebruiken moet je "include DbGeneralFunctions;" Toevoegen aan je code! //
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-require_once "./DbGeneralFunctions.php"; 
+require_once "/Database/DbGeneralFunctions.php"; 
 
 
 // Creates een nieuwe student
