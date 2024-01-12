c58967b0f526ff677aa48431bf70ccb5e40ba0e9
Jeffrey <82870441+kw1c-KuijpersJeffrey@users.noreply.github.com>
2024-01-12 14:48:16 +0100

    RegisterForm done

--- process_form.php
+++ process_form.php
@@ -1,25 +1,23 @@
 <?php
-
 require_once 'Database/DatabaseFunctions.php';
 
 $groepName = $_POST['team_name'];
 $email = $_POST['email'];
-$reserveerDatumTijdstr = "2024-01-" . $_POST['day'] . " " . $_POST['time'] . ":" . "00";
+$reserveerDatumTijdstr = $_POST['time'];
+
 $reserveerDatumTijd = new DateTime($reserveerDatumTijdstr);
 
 $result = $reserveerDatumTijd->format('Y-m-d H:i:s');
-
-
-#CreateGroep($groepName, $email, $result);
+CreateGroep($groepName, $email, $result);
 
 $groepId = ReadOneGroepName($groepName);
 
 
 $members = [$_POST['member1'], $_POST['member2'], $_POST['member3']];
-if (isset($_POST['member4'])) {
+if (($_POST['member4']) !== "") {
     array_push($members, $_POST['member4']);
 }
-if (isset($_POST['member4'])) {
+if (($_POST['member5']) !== "") {
     array_push($members, $_POST['member5']);
 }
 
@@ -27,14 +25,5 @@ if (isset($_POST['member4'])) {
 foreach ($members as $member) {
     CreateStudent($groepId, $member);
 }
-
-
-
-
-
-
-
-
-
-
-
+echo 'Registratie successfull. U wordt terug gestuurd naar de home pagina over een paar seconden';
+header( "refresh:4;url=index.php" );
