8196b2e2e89fe26397db1ed4b178465ab4d84e2d
Nuuradiin21 <nuuradiinfg12@gmail.com>
2024-01-15 22:52:39 +0100

    Test for save

--- process_form.php
+++ process_form.php
@@ -1,32 +1,55 @@
 <?php
-
-require_once './Database/GroepController.php';
+include "Database/GroepController.php";
 include "Database/StudentController.php";
 
-$groepName = $_POST['team_name'];
-$email = $_POST['email'];
-$reserveerDatumTijdstr = $_POST['time'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit_form"])) {
+    // Get form data
+    $groupNaam = $_POST["team_name"];
+    $email = $_POST["email"];
+    $selectedTime = $_POST["time"];
+    $member1 = $_POST["member1"];
+    $member2 = $_POST["member2"];
+    $member3 = $_POST["member3"];
+    $member4 = $_POST["member4"];
+    $member5 = $_POST["member5"];
 
-$reserveerDatumTijd = new DateTime($reserveerDatumTijdstr);
+    // Check if group name already exists
+    $existingGroup = ReadOneGroepName($groupNaam);
 
-$result = $reserveerDatumTijd->format('Y-m-d H:i:s');
-CreateGroep($groepName, $email, $result);
+    if ($existingGroup) {
+        // Group name already exists, display an error message
+        echo '<p class="warning">Group with the name ' . $groupNaam . ' already exists. Please choose a different name.</p>';
+    } else {
+        // Continue with the registration process
+        // Check if email already exists
+        $existingEmail = CheckIfEmailExists($email);
 
-$groepId = ReadOneGroepName($groepName);
+        if ($existingEmail) {
+            // Email already exists, display an error message
+            echo '<p class="warning">Email ' . $email . ' is already associated with another group. Please use a different email.</p>';
+        } else {
+            // Check if the selected date and time are already reserved
+            $isTimeReserved = CheckIfTimeReserved($selectedTime);
 
+            if ($isTimeReserved) {
+                // Time is already reserved, display an error message
+                echo '<p class="warning">The selected date and time (' . $selectedTime . ') are already reserved by another group. Please choose a different time.</p>';
+            } else {
+                // Continue with the registration process
+                // Insert group and student data into the database
+                // Call CreateGroep and CreateStudent functions as needed
+                CreateGroep($groupNaam, $email, $selectedTime);
+                $groupId = ReadOneGroepName($groupNaam);
+                CreateStudent($groupId, $member1);
+                CreateStudent($groupId, $member2);
+                CreateStudent($groupId, $member3);
+                if (!empty($member4)) CreateStudent($groupId, $member4);
+                if (!empty($member5)) CreateStudent($groupId, $member5);
 
-$members = [$_POST['member1'], $_POST['member2'], $_POST['member3']];
-if (($_POST['member4']) !== "") {
-    array_push($members, $_POST['member4']);
+                // Display success message or redirect to another page
+                echo '<p class="success">Registration successful!</p>';
+            }
+        }
+    }
 }
-if (($_POST['member5']) !== "") {
-    array_push($members, $_POST['member5']);
-}
-
-
-foreach ($members as $member) {
-    CreateStudent($groepId, $member);
-}
-header( "refresh:4;url=index.php" );
-echo 'Registratie successfull. U wordt terug gestuurd naar de home pagina over een paar seconden';
-
+?>
