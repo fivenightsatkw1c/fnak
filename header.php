bf9eace710961aad2194ef6cce1c351b4b06fdeb
Nuuradiin21 <nuuradiinfg12@gmail.com>
2024-01-19 14:12:17 +0100

    Change and ad the index back

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ if(!isset($link3)){
                 <div class="nav-menu" id="nav-menu">
                     <ul class="nav-list grid">
                         <li class="nav-item">
-                            <a href="RegisterForm.php" class=<?php echo "\"nav-link ".$link1."\"" ?>>
+                            <a href="Index.php" class=<?php echo "\"nav-link ".$link1."\"" ?>>
                                 <i class="uil uil-estate nav-icon"></i>Registreren
                             </a>
                         </li>
